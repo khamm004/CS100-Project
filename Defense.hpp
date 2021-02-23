7abfd6e611daaea9e0d17458da0ad1f6b33462bc
estela-ramirez <ramirezestela01@gmail.com>
2021-02-22 20:31:47 -0800

    added getter/setter methods

--- Defense.hpp
+++ Defense.hpp
@@ -1,8 +1,22 @@
+#ifndef __DEFENSE_HPP__
+#define __DEFENSE_HPP__
+
+#include <string>
 #include<iostream> 
 
 class Defense{
     private:
 	int protectionLevel;
     private:
+	void setProtecttionLevel(int lvl){
+		protectionLevel = lvl;
+	}
+	
+	void getProtectionLevel(){
+		return protectionLevel;
+	}
+
 	virtual void defend() = 0;
 };
+
+#endif //__DEFENSE_HPP__
