b71291d9dec51ddb792a806a6e8006410a17f44b
khamm004 <khamm004@ucr.edu>
2021-02-24 15:57:56 -0800

    Created Stab class
    * Strategy pattern
    * Implemented attack method functionality for enemy (troll)

---
+++ header/stab.hpp
@@ -0,0 +1,21 @@
+#ifndef __STAB_HPP__
+#define __STAB_HPP__
+#include <iostream>
+#include <cstring>
+
+class Stab: public Attack
+{
+public:
+    virtual std::string attack(){
+        srand(time(NULL));
+        if(rand()%2){
+            std::cout << "You have successfully stabbed the enemy \n they are significantly wounded \n"
+        }
+        else{
+            std::cout << "Your attempt to stab the enemy was not successful \n the enemy remains strong \n"
+        }
+    }
+};
+
+#endif //__STAB_HPP__
+
