1be49e315ebd3553f11d49db45bcbc3c5dbced2f
khamm004 <khamm004@ucr.edu>
2021-02-24 15:37:27 -0800

    Created Enchant class
    * Part of Strategy Pattern
    * implemented attack method functionality

---
+++ header/enchant.hpp
@@ -0,0 +1,20 @@
+#ifndef __ENCHANT_HPP__
+#define __ENCHANT_HPP__
+#include <iostream>
+#include <cstring>
+
+class Enchant: public Attack
+{
+public:
+    virtual std::string attack(){
+	srand(time(NULL));
+	if(rand()%2){
+	    std::cout << "You have successfully enchanted the enemy \n they will be blinded for 5 seconds \n"
+	}
+	else{
+	    std::cout << "The enchantment was not successful \n the enemy remains strong \n"
+	} 
+    }
+};
+
+#endif //__ENCHANT_HPP__
