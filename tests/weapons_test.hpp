e678f5a406b3eada465da84a965fffb35bb5b317
bijalm <02bijal@gmail.com>
2021-02-25 08:27:43 -0800

    Fixed memory leaks

--- tests/weapons_test.hpp
+++ tests/weapons_test.hpp
@@ -16,47 +16,54 @@ TEST(DamageTest, ZeroInputDamage){
         Weapons* test = new Wand();
         test->setDamage(0);
         EXPECT_EQ(test->getDamage(), 0);
+	delete test;
 }
 
 TEST(DamageTest, ValidInputDamage){
         Weapons* test = new Wand();
         test->setDamage(40);
         EXPECT_EQ(test->getDamage(), 40);
+	delete test;
 }
 
 TEST(DamageTest, InvalidInputDamage){
+	Weapons* test = new Wand();
 	EXPECT_THROW({
         	try {
-            		Weapons* test = new Wand();
 	    		test->setDamage(-2);
-        	}	
+		}	
         	catch (std::invalid_argument& ia)
         	{
             		EXPECT_STREQ("-2", ia.what());
            		throw;
        		}
    	 }, std::invalid_argument);
+	delete test;
 }
 //Checking to ensure each constructor works correctly
 
 TEST(WandTest, ConstructorAccuracy){
         Weapons* test = new Wand();
         EXPECT_EQ(test->getDamage(), 20);
+	delete test;	
 }
 
 TEST(SwordTest, ConstructorAccuracy){
         Weapons* test = new Sword();
         EXPECT_EQ(test->getDamage(), 25);
+	delete test;
 }
 
 TEST(BowAndArrowTest, ConstructorAccuracy){
         Weapons* test = new BowAndArrow();
         EXPECT_EQ(test->getDamage(), 15);
+	delete test;
 }
 
 TEST(AxeTest, ConstructorAccuracy){
         Weapons* test = new Axe();
         EXPECT_EQ(test->getDamage(), 10);
+	delete test;
 }
 
 #endif //__WEAPONS_TESTS_HPP__
