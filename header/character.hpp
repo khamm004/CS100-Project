db0bd33c47bf5acebef19662fd87b819c63614bc
estela-ramirez <ramirezestela01@gmail.com>
2021-02-27 18:28:55 -0800

    add private variables/ getter/setter

--- header/character.hpp
+++ header/character.hpp
@@ -6,19 +6,32 @@
 class Character
 {
 private:
+	Weapon* weapon;
+	Defense* defense;
 	Attack* attack;
+	int health;
 
 public:
 	~Character(){
 		delete attack;
 	}
+
 	Attack* GetAttack(){
 		return this->attack;
 	}
+
 	void SetAttack(Attack* atk){
 		attack = atk;
+	}
+	
+	void setHealth(int h){
+		health = h;
+	}	
+
+	int getHealth(){
+		return health;
 	}
 
 };
 
-#endif //_ _CHARACTER_HPP__
+#endif //__CHARACTER_HPP__
