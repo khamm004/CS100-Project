99331a78955fb062220f8e1d13df84e04f56e5e6
bijalm <02bijal@gmail.com>
2021-03-09 23:10:37 -0800

    Added desert tree function

--- src/gameTree.cpp
+++ src/gameTree.cpp
@@ -139,24 +139,84 @@ void GameTree::beachInit() {
 	gameNodes.push_back(node10);
 }
 
-/*
 void GameTree::desertInit() {
-	GameNode *node0 = new GameNode("");
-	GameNode *node1 = new GameNode();
-	GameNode *node2 = new GameNode();
-	GameNode *node3 = new GameNode();
-	GameNode *node4 = new GameNode();
-	GameNode *node5 = new GameNode;
-	GameNode *node6 = new GameNode;
-	GameNode *node7 = new GameNode;
-	GameNode *node8 = new GameNode;
-	GameNode *node9 = new GameNode;
-	GameNode *node10 = new GameNode;
-	GameNode *node11 = new GameNode;
+	GameNode *node0 = new GameNode("You continue going down the same path straight ahead.\nYou encounter a bridge blocked by talking sheep that ask a riddle for passage.\nWhat does the fox say?");
 
-	node0->UserOptions.push_back(UserOption("", ));
+	GameNode *node1 = new GameNode("You passed by sheep and walked across the bridge. As you continue down the path you find yourself walking between rows of tall, ominous trees.\nThough it is the middle of the day, most of the suns rays are blocked by the thick foliage. You continue down the road and begin to hear whispers emanating from within the forest and a troll emerges from the forest, blocking your path. \nYou can either:");
+
+	GameNode *node2 = new GameNode("You have successfully defeated the troll!\nIm tall when Im young, and Im short when Im old. What am I?");
+
+	GameNode *node3 = new GameNode("You sprint into the forest leaving the lumbering troll behind. With no way of knowing where\n to go next to get back on track toward the Desert Oasis you search for a friendly creature\n in the wilderness to help you on your way. You finally come across an owl perched on a tree branch near a troll that you can ask for directions.\n But to be able to ask the owl, you must combat with troll. ");
+
+	GameNode *node4 = new GameNode("Nice job! You are too smart to be tripped up with those simple riddles. You notice \nyou are now longer in the forest. You are across a field of volcanic rocks. Some are\noozing lava and some look like normal rocks. Uh oh! There is a troll following you\n from the forest, but you dont have enough time to look for an alternative route. \nYou can either.. ");
+
+	GameNode *node5 = new GameNode("You were able to survive against the fight with the troll and helped out the owl! You ask the owl if it can \nhelp show you the way to the Desert Oasis. The owl agreed but only if you are able to solve the riddle. \nHow can a man go eight days without sleep?");
+
+	GameNode *node6 = new GameNode("I have lakes with no water, mountains with no stone and cities with no buildings. What am I?");
+
+	GameNode *node7 = new GameNode("That was a close one! As you look ahead you are able to see some vegetation.\nThat means you are nearly at the Oasis! However, you do not have enough strength to carry you over there.\nLuckily for you there is a talking camel that is willing to take you there if you are able to solve the following riddle\nWhat should you break before you use it?");
+
+	GameNode *node8 = new GameNode("Well done, you are a true fighter indeed. No troll will be messing with you anytime soon.\n As you continue to walk in victory you notice some cactus! Your mouth begins\n to water at the idea of drinking the water inside. You are about to reach for it, but a troll\n is going to get it before you, unless you defeat it.");
+	
+	GameNode *node9 = new GameNode("The owl begins to fly with you to show you the way to the Desert Oasis.\n The owl stops and tells you that the Desert Oasis is in front of them. The owl flys away and as\n you watch it fly away, the troll had come back to block you from going past it.");
+
+	GameNode *node10 = new GameNode("You were unable to successfully escape the troll. But you are super close to your destination! Your last step is to fight against the troll!");
+
+
+	node0->userOptions.push_back(UserOption("Wa-pa-pa-pa-pa-pa-pow", 4, nullptr));
+	node0->userOptions.push_back(UserOption("Hatee-hatee-hatee-ho", 4, nullptr));
+	node0->userOptions.push_back(UserOption("Fraka-kaka-kaka-kaka-kow", 4, nullptr));
+	node0->userOptions.push_back(UserOption("All of the Above", 0, node1));
+	gameNodes.push_back(node0);
+
+	node1->userOptions.push_back(UserOption("Combat with the troll", 1, node2));
+	node1->userOptions.push_back(UserOption("Take a longer route through forest, but be warned it will take a toll on your health by 5 points", 2, node3));
+	gameNodes.push_back(node1);
+
+	node2->userOptions.push_back(UserOption("A tomato", 4, nullptr));
+	node2->userOptions.push_back(UserOption("Yo-yo", 4, nullptr));
+	node2->userOptions.push_back(UserOption("Cold", 4, nullptr));
+	node2->userOptions.push_back(UserOption("A candle", 0, node4));
+	gameNodes.push_back(node2);
+
+	node3->userOptions.push_back(UserOption("Combat with the troll", 1, node5));
+	node3->userOptions.push_back(UserOption("Throw a stick to distract the troll to escape, but be warned the troll can come back and hurt you and make you will lose 10 points of health", 2, node6));
+	gameNodes.push_back(node3);
+
+	node4->userOptions.push_back(UserOption("Combat with the troll", 1, node7));
+	node4->userOptions.push_back(UserOption("Jump over the volcanic rocks, lose 15 points of health", 2, node8));
+	gameNodes.push_back(node4);
+
+	node5->userOptions.push_back(UserOption("By sleeping at night", 0, node9));
+	node5->userOptions.push_back(UserOption("Energy drinks", 4, nullptr));
+	node5->userOptions.push_back(UserOption("Meditation", 4, nullptr));
+	node5->userOptions.push_back(UserOption("By not sleeping at all", 4, nullptr));
+	gameNodes.push_back(node5);
+
+	node6->userOptions.push_back(UserOption("Grass", 4, nullptr));
+	node6->userOptions.push_back(UserOption("Forest", 4, nullptr));
+	node6->userOptions.push_back(UserOption("Ocean", 4, nullptr));
+	node6->userOptions.push_back(UserOption("map", 0, node10));
+	gameNodes.push_back(node6);
+
+	node7->userOptions.push_back(UserOption("glowsticks", 4, nullptr));
+	node7->userOptions.push_back(UserOption("egg", 3, nullptr));
+	node7->userOptions.push_back(UserOption("spaghetti", 4, nullptr));
+	node7->userOptions.push_back(UserOption("kit-kat bar", 4, nullptr));
+	gameNodes.push_back(node7);
+
+	node8->userOptions.push_back(UserOption("It is time to fight!", 5, nullptr));
+	gameNodes.push_back(node8);
+
+	node9->userOptions.push_back(UserOption("Fight and you will finally reach your destination!", 5, nullptr));
+	gameNodes.push_back(node9);
+
+	node10->userOptions.push_back(UserOption("Become victorious!", 5, nullptr));
+	gameNodes.push_back(node10);
 }
 
+
+/*
 void GameTree::forestInit() {
 	GameNode *node0 = new GameNode();
 	GameNode *node1 = new GameNode();
