609f1bc9a41ed7086ccc4353a8b05e9e8768be32
estela-ramirez <ramirezestela01@gmail.com>
2021-03-10 08:46:13 -0800

    created main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../header/menu.hpp"
+#include "../character.hpp"
+#include "../header/fairy.hpp"
+#include "../header/elf.hpp"
+#include "../header/knight.hpp"
+
+void start(){
+	MainMenu();
+}
+
+
