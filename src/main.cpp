55c8e4a7381ffd33fa12eed6a3ed3a2b29db5fcf
Estela Ramirez <ramirezestela01@gmail.com>
2021-03-09 18:04:21 -0800

    add choose character

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
-using namespace std;
+#include <isotream>
 #include "../header/menu.hpp"
+#include "../character.hpp"
+#include "../header/fairy.hpp"
+#include "../header/elf.hpp"
+#include "../header/knight.hpp"
 
-int main(){
-	MainMenu();
-	return 0;
+void start(){
+	Character * userChar = nullptr;
+	displayCharacter();
+	int choice = getChoice();
+	userChar = selectCharacter(choice);
+	if (userChar != nullptr){
+		//main convo
+		string name;
+		cout << "What is your name?" << endl;
+		cin >> name;
+	        cout << "Good luck on your travels " << name << endl;
+		MainMenu();
+		delete userChar;
+	}	
 }
