7e4b94cbad2209e939169e9e0cdd5749c0968958
estela-ramirez <ramirezestela01@gmail.com>
2021-03-10 08:42:27 -0800

    start menu.hpp

--- header/menu.hpp
+++ header/menu.hpp
@@ -1,2 +1,16 @@
 #ifndef __MENU_HPP__
 #define __MENU_HPP__
+
+#include <iostream>
+#include "CharacterFactory.hpp"
+#include "FairyFactory.hpp"
+#include "KnightFactory.hpp"
+#include "TrollFactory.hpp"
+#include "ElfFactory.hpp"
+
+#include "character.hpp"
+#include "knight.hpp"
+#include "fairy.hpp"
+#include "elf.hpp"
+#include "troll.hpp"
+using namespace std;
